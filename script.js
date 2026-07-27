42a565e787478d733ed18f781140540107337f60
wedding-invitation <web.hostinger.27@gmail.com>
2026-07-27 10:55:06 -0500

    Fix typo in guest entry for Janis Burke

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ const guests = {
     allowedGuests: 2,
     invitedGuests: ["Randy Henderson", "Kim Henderson"]
   },
-    "janis-burke": {
+    "janis-burkle": {
     displayName: "Peter & Janis Burke",
     allowedGuests: 2,
     invitedGuests: ["Peter Burke","Janis Burke"]
